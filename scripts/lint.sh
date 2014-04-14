931495f0056e448a27c81848f71ac3bb0ab3a84d
Yunong Xiao <yunong@joyent.com>
2014-04-14 23:59:45 +0000

    add linter and styler configs

---
+++ scripts/lint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o xtrace
+
+git submodule update
+
+cd ./deps/javascriptlint/
+make install
+cd ../..
+
+./deps/javascriptlint/build/install/jsl --conf ./tools/jsl.node.conf \
+    ./*.js ./bin/*.js ./test/*.js
+
+./deps/jsstyle/jsstyle -f ./tools/jsstyle.conf ./*.js ./bin/*.js ./test/*.js
